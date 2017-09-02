f70f90ebcd735d5fce3312694c222f10bb9ea01c
EC2 Default User <ec2-user@ip-172-31-31-81.us-west-1.compute.internal>
2017-09-02 23:31:14 +0000

    Add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+// content of index.js
+const http = require('http')  
+const port = 80
+
+const requestHandler = (request, response) => {  
+  console.log(request.url)
+  response.end('Hello, I am a Node.js Server! Welcome!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
+
