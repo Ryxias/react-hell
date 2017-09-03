1bcd1d56181db2b0e6c26a17e38bdab75966129c
Derek Wang <derek.wang@airbnb.com>
2017-09-02 20:21:49 -0700

    Set up some primitive ip blacklist

--- index.js
+++ index.js
@@ -3,12 +3,33 @@
 //
 //   Global front controller for this project!
 //
+'use strict';
+
 const http = require('http');
 const port = 80;
 
 const requestHandler = (request, response) => {
-  console.log(request.url);
-  response.end('Hello, I am a Node.js Server! Welcome!')
+  // Log information about the request
+  console.log('----');
+  console.log(request.connection.remoteAddress);
+  console.log('  ' + request.method + ' ' + request.url);
+  console.log('----');
+
+  if (isBlacklisted(request)) {
+    response.end('Hello, I am a Node.js Server!'); // Show them a dead site
+  } else {
+    response.end('Hello, I am a Node.js Server, welcome!');  // For non-blacklisted users, show them everything
+  }
+};
+
+const isBlacklisted = (request) => {
+  let ipblacklist = [
+    '::ffff:84.198.14.161', // Some scanner
+
+    //'::ffff:76.218.105.69', // Tiffany's ip address, only use for testing
+  ];
+
+  return ipblacklist.includes(request.connection.remoteAddress);
 };
 
 const server = http.createServer(requestHandler);
