a1a6f6a2de47f387b607be25948a0f8dbd892300
David Zou <david.zou@outlook.com>
2017-09-03 04:20:40 -0700

    [setup] setup React project in progress, change folder structure (suggestion)

---
+++ client/index.js
@@ -0,0 +1,4 @@
+// Main index.js file, this is where you render the React Virtual DOM
+// i.e.
+// ReactDOM.render(<App/>, document.getElementById('app'));
+// where <App/> is the root component of the application
