9ffc431dae0256ab7a7388f1d0c3f4f78e8cdf27
Derek Wang <derek.wang@airbnb.com>
2017-09-02 18:46:49 -0700

    Add some scripts for deployment and server execution

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-// content of index.js
-const http = require('http')  
-const port = 80
+//
+// index.js
+//
+//   Global front controller for this project!
+//
+const http = require('http');
+const port = 80;
 
-const requestHandler = (request, response) => {  
-  console.log(request.url)
+const requestHandler = (request, response) => {
+  console.log(request.url);
   response.end('Hello, I am a Node.js Server! Welcome!')
-}
+};
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
-server.listen(port, (err) => {  
+server.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
 
   console.log(`server is listening on ${port}`)
-})
+});
 
