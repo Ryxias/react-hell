fc56f64f8920050c7196880426f2433b59765e92
Derek Wang <derek.wang@airbnb.com>
2017-09-02 22:55:19 -0700

    Adds sequelize configurations, adds pug as templating engine, adds basic index template

--- server/routes/base.js
+++ server/routes/base.js
@@ -2,14 +2,11 @@ module.exports = app => {
 
   // Welcome page
   app.get('/', (req, res, next) => {
-    // return res.render('home', {
-    //   header: true,
-    //   footer: true,
-    //   title: 'CSPA - Computer Science Proficiency Assessment',
-    //   css: ['home.css'],
-    //   selected_about: true,
-    // })
-
-    return res.send("Hello!");
+    return res.render('index',
+      {
+        title: 'Hello!',
+        message: 'Home of the Chuuni'
+      }
+    )
   });
 };
