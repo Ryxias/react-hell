e304f9423d6d3c588579c39c4c6c897222e005eb
Derek Wang <derek.wang@airbnb.com>
2017-09-03 18:46:00 -0700

    Add primitive SIF page that displays an image of random SIF card

--- server/routes/base.js
+++ server/routes/base.js
@@ -9,4 +9,38 @@ module.exports = app => {
       }
     )
   });
+
+  // Love Live Gacha crap
+  app.get('/sif/random', (req, res, next) => {
+
+    const http = require('http');
+
+    const MAX_CARD_ID = 1234;
+    let card_id = Math.floor(Math.random() * MAX_CARD_ID);
+    http.get(
+      {
+        hostname: 'schoolido.lu',
+        port: 80,
+        path: '/api/cards/' + card_id + '/',
+      },
+      (result) => {
+        let data = '';
+        result.on('data', (chunk) => {
+          data += chunk;
+        });
+        result.on('end', () => {
+          let card_data = JSON.parse(data);
+          let card_name = card_data['idol']['name'];
+          let card_image_url = 'http:' + card_data['card_image'];
+
+          return res.render('sif',
+            {
+              card_name: card_name,
+              card_image_url: card_image_url,
+            }
+          );
+        });
+      }
+    );
+  });
 };
