06f395a2f3a1412ad703ae1e9caaecd42429c69d
Derek Wang <derek.wang@airbnb.com>
2017-09-05 23:02:51 -0700

    Redirects all traffic to HTTPS.  Fixes TLS problem with http images to /sif

--- server/server.js
+++ server/server.js
@@ -35,6 +35,26 @@ global.db = new Sequelize(
   }
 );
 
+// Add a listener that redirects incoming HTTP requests to HTTPS
+app.use((req, res, next) => {
+  // As it turns out this is extremely not-trivial since the environments upon which this
+  // application operates are behind TLS-terminating devices, which forward the request
+  // as HTTP.
+  //
+  // Instead, we rely on a specific headers that the services provide.  AWS ELB provides
+  // a header called X-FORWARDED-PROTO
+  // @see https://www.allcloud.io/how-to/how-to-force-https-behind-aws-elb/
+  //
+  // The caveat here is that `req.protocol` is permanently broken :(
+  if (req.header('x-https-protocol') // Sometimes Nginx
+    || req.header('x-forwarded-proto') === 'https' // AWS ELB
+  ) {
+    next();
+    return;
+  }
+  res.redirect('https://' + req.hostname + req.originalUrl);
+});
+
 
 //
 // Build routes
