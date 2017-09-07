4a3c7569f8a88e6955d4ae7ddeaeb0b1627a9d9b
Derek Wang <derek.wang@airbnb.com>
2017-09-07 00:02:14 -0700

    Restructures the JS to make the UI a little bit more snappy

--- public/js/sif.js
+++ public/js/sif.js
@@ -1,19 +1,22 @@
 (() => {
   $(document).ready(() => {
+    const $closed_envelope = $(".envelope-closed");
+    const $open_envelope = $(".envelope-open");
+    const $data_blob = $(".data");
+    const audio = new Audio($data_blob.data("open-sound-url"));
 
     // Put an onclick listener on the close envelope that replaces it
     // with the open envelope, which then animates itself to grow fat as fuck
     // before exploding in disappointment for all you gacha fools
 
-    $(".envelope-closed").on("click", () => {
-      let $closed_envelope = $(".envelope-closed");
-      let $open_envelope = $(".envelope-open");
-      let $data_blob = $(".data");
-
+    $closed_envelope.on("click", () => {
       // Play kinky music
-      let audio = new Audio($data_blob.data("open-sound-url"));
-      audio.play();
+      //   Putting the animation crap in here ensures the audio begins to play before
+      //   the animations, making the UI "feel" more snappy.
+      audio.play().then(animateOpeningBox);
+    });
 
+    const animateOpeningBox = () => {
       // Replace the closed envelope with the open one
       $closed_envelope.addClass("hide");
       $open_envelope.removeClass("hide");
@@ -26,6 +29,6 @@
         $container.css("display", "none").removeClass("hide");
         $container.fadeIn("slow");
       });
-    });
+    };
   });
 })();
