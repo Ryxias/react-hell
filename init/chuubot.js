8030501d2d0589f35eacb8579c0fadbb1d904ade
Derek Wang <derek@thirdimpact.co>
2017-09-11 23:59:25 -0700

    Chuubot gacha enhancement

--- init/chuubot.js
+++ init/chuubot.js
@@ -1,18 +1,34 @@
+'use strict';
 //
 // Connect Chuubot and provide an example of how to create a listener
 //
-
 module.exports = (config) => {
+  const LoveLiveClient = require(PROJECT_ROOT + '/lib/love_live_client');
+  const ll_client = new LoveLiveClient();
   const chuu = require(PROJECT_ROOT + '/lib/slackbot_framework')(config.slack);
 
-// Add dumb listener for baachuu
+  // Add dumb listener for baachuu
   chuu.on(/chuu/, (message, send) => { send('baaaaaaaaaa'); });
 
-// Add the
-  chuu.on(/!gacha/, (message, send) => {
-    const LoveLiveClient = require(PROJECT_ROOT + '/lib/love_live_client');
-    let ll_client = new LoveLiveClient();
-    ll_client.gachaRCard().then((card) => {
+  // gacha
+  //   Provide `!gacha`
+  //   You can add 'sr' or 'aqours' to change the mode of the bot
+  chuu.on(/^!gacha/, (message, send) => {
+    let message_text = message.text.toLowerCase();
+
+    let unit = 'mus';
+    if (message_text.includes('aqours')) {
+      unit = 'aqours';
+    }
+
+    let promise;
+    if (message_text.includes('sr')) {
+      promise = ll_client.gachaSRCard(unit);
+    } else {
+      promise = ll_client.gachaRCard(unit);
+    }
+
+    promise.then((card) => {
       send('[' + card.getId() + '] ' + card.getName() + ' - ' + card.getImageUrl());
     });
   });
