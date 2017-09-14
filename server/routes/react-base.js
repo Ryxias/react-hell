f2c9910a5d6302c0b626c795b456cb9ec6e5f92d
David Zou <david.zou@outlook.com>
2017-09-13 23:55:05 -0700

    [hotfix] need to add more packages for webpack to work

--- server/routes/react-base.js
+++ server/routes/react-base.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 
 // Welcome page
 router.get('/', (req, res, next) => {
-  res.sendfile('index.html', { root: `${__dirname}/../../public_react` })
+  res.sendFile('index.html', { root: `${__dirname}/../../public_react` })
 });
 
 module.exports = router;
