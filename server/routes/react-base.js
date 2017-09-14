626f3d26f03cd9df34f519d04eb417b7432c59e8
David Zou <david.zou@outlook.com>
2017-09-13 23:42:20 -0700

    [typo] fix arrow function in react-base.js

--- server/routes/react-base.js
+++ server/routes/react-base.js
@@ -3,7 +3,7 @@
 const router = require('express').Router();
 
 // Welcome page
-router.get('/', (req, res, next) -> {
+router.get('/', (req, res, next) => {
   res.sendfile('index.html', { root: `${__dirname}/../../public_react` })
 });
 
