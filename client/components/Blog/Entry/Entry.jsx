b91d845ae91d2eff9fc6c2435e27f4280e81110b
David Zou <david.zou@outlook.com>
2017-09-26 20:00:50 -0700

    [wip] testing out entries for blog with plain text

--- client/components/Blog/Entry/Entry.jsx
+++ client/components/Blog/Entry/Entry.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const BlogEntry = ({ testText }) => {
+const BlogEntry = ({ testText, blogPost }) => {
   return (
     <div>
       There should be text here: { testText }
+      <h3>{blogPost.title}</h3>
+      <b>Date:</b> {blogPost.date}<br></br>
+      <div>
+        {blogPost.text}
+      </div>
     </div>
   );
 };
