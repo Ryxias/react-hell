2daabe420b9c00678e2f2bec5c8118a5cbadd961
David Zou <david.zou@outlook.com>
2017-09-29 15:35:54 -0700

    [wip] working on model schema for BlogPost

---
+++ model/BlogPosts.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const { STRING, TEXT, DATE, BIGINT } = require('sequelize');
+
+module.exports = sequelize => {
+  return sequelize.define('blogpost', {
+    id: {
+      type: BIGINT.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    createdAt: DATE,
+    updatedAt: DATE,
+    author: {
+      type: STRING(255),
+      allowNull: false,
+    },
+    title: {
+      type: STRING(255),
+      allowNull: false,
+    },
+    body: {
+      type: TEXT,
+      allowNull: false,
+    },
+    hooks: {},
+    instanceMethods: {},
+  });
+};
