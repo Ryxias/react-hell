d7fe395c2abd071b3b0e31a6ec7030b94aaf39c7
David Zou <david.zou@outlook.com>
2017-11-12 21:03:07 -0800

    [wip] continuing to test css transitions

--- client/components/Gacha/GachaContent.jsx
+++ client/components/Gacha/GachaContent.jsx
@@ -4,29 +4,37 @@ import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import GachaButtons from './GachaButtons.jsx';
 
 const GachaContent = ({ gameState, getGacha, openEnvelope }) => {
-  const envelopeClosedClasses = ['envelope-image envelope-closed'];
-  const envelopeOpenedClasses = ['envelope-image envelope-open'];
-
-  if (gameState.envelopeIsOpened) {
-    envelopeClosedClasses.push('hide');
-    envelopeOpenedClasses.push('reveal');
-    console.log('transitioning to opened');
-    console.log('what is envelopeOpened now:', envelopeOpenedClasses.join(' '));
-    console.log('what is envelopeClosed now:', envelopeClosedClasses.join(' '));
-  } else {
-    envelopeClosedClasses.push('reveal');
-    envelopeOpenedClasses.push('hide');
-    console.log('transitioning to closed');
-    console.log('what is envelopeOpened now:', envelopeOpenedClasses.join(' '));
-    console.log('what is envelopeClosed now:', envelopeClosedClasses.join(' '));
-  }
+  // const envelopeClosedClasses = ['envelope-image envelope-closed'];
+  // const envelopeOpenedClasses = ['envelope-image envelope-open'];
+  //
+  // if (gameState.envelopeIsOpened) {
+  //   envelopeClosedClasses.push('hide');
+  //   envelopeOpenedClasses.push('reveal');
+  //   console.log('transitioning to opened');
+  //   console.log('what is envelopeOpened now:', envelopeOpenedClasses.join(' '));
+  //   console.log('what is envelopeClosed now:', envelopeClosedClasses.join(' '));
+  // } else {
+  //   envelopeClosedClasses.push('reveal');
+  //   envelopeOpenedClasses.push('hide');
+  //   console.log('transitioning to closed');
+  //   console.log('what is envelopeOpened now:', envelopeOpenedClasses.join(' '));
+  //   console.log('what is envelopeClosed now:', envelopeClosedClasses.join(' '));
+  // }
 
   return (
     <div className="gacha-container">
       <div className="envelope-image-container">
-        <ReactCSSTransitionGroup transitionEnterTimeOut={3000}>
-          <img className={envelopeClosedClasses.join(' ')} onClick={openEnvelope} src={"/statics/i/" + gameState.envelope_image_closed} />
-          <img className={envelopeOpenedClasses.join(' ')} src={"/statics/i/" + gameState.envelope_image_open} />
+        <ReactCSSTransitionGroup
+          transitionName="reveal"
+          transitionEnterTimeout={450}
+          transitionLeave={false}
+          transitionAppear={true}
+          transitionAppearTimeout={450}>
+          {
+            gameState.envelopeIsOpened ?
+            <img key={1} className="envelope open" src={"/statics/i/" + gameState.envelope_image_open} /> :
+            <img key={1} className="envelope closed" onClick={openEnvelope} src={"/statics/i/" + gameState.envelope_image_closed} />
+          }
         </ReactCSSTransitionGroup>
       </div>
       <div className="opened-card-container hide">
