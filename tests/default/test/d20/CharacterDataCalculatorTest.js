c1a6bf56b4ac4e925af7875b3e0fdec75fd74e76
Derek Wang <derek@thirdimpact.co>
2017-11-19 22:50:00 -0800

    Incorporates gear into the stat calculations

--- tests/default/test/d20/CharacterDataCalculatorTest.js
+++ tests/default/test/d20/CharacterDataCalculatorTest.js
@@ -12,26 +12,102 @@ describe('CharacterData', function() {
 
       // this value isn't defined in the template.  Should be derived with recalculateAll()
       subject.recalculateAll();
+
       assert.equal(subject.getParameter('foundation:ability_modifiers:strength'), 2);
     });
 
     it('should be able to calculate ability scores and modifiers', function() {
       const subject = CharacterData.getTemplate();
 
-      subject.setParameter('adjustment:ability_scores:strength:enhancement', 2);
+      subject.setParameter('adjustment:ability_scores:strength', 2);
 
-      assert.equal(subject.getParameter('adjustment:ability_scores:strength:enhancement'), 2);
+      assert.equal(subject.getParameter('adjustment:ability_scores:strength'), 2);
       assert.equal(subject.getParameter('foundation:ability_scores:strength'), 15);
       assert.equal(subject.getParameter('accumulation:ability_scores:strength'), 17);
       assert.equal(subject.getParameter('foundation:ability_modifiers:strength'), 3);
     });
 
-    it.only('should be able to accumulate foundation hp from class and race', function() {
+    it('should be able to accumulate foundation hp from class, race, and constitution', function() {
       const subject = CharacterData.getTemplate();
 
       subject.recalculateAll();
       assert.equal(subject.getParameter('foundation:hit_points'), 22); // 10 + 7 + 2 (+1 con x 3 levels)
     });
-  });
 
+    it('should be able to accumulate base attack bonus', function() {
+      const subject = CharacterData.getTemplate();
+
+      subject.recalculateAll();
+      assert.equal(subject.getParameter('foundation:base_attack_bonus'), 2);
+    });
+
+    it('should be able to accumulate combat maneuver stats', function() {
+      const subject = CharacterData.getTemplate();
+
+      subject.recalculateAll();
+      assert.equal(subject.getParameter('accumulation:combat_maneuver:bonus'), 3);
+      assert.equal(subject.getParameter('accumulation:combat_maneuver:defense'), 15);
+      assert.equal(subject.getParameter('accumulation:attack_bonus:melee'), 5);
+      assert.equal(subject.getParameter('accumulation:attack_bonus:ranged'), 5);
+    });
+
+    it('should be able to accumulate equipment adjustments', function() {
+      const subject = CharacterData.getTemplate();
+
+      subject.recalculateAll();
+
+      const value = subject.getParameter('adjustment.equipment.bonus_breakdowns.wisdom');
+      assert.equal(value.enhancement, 2);
+      assert.equal(subject.getParameter('adjustment.equipment.bonuses.wisdom'), 2);
+    });
+
+    it('should be able to incorporate equipment bonuses into actual ability scores', function() {
+      const subject = CharacterData.getTemplate();
+
+      subject.recalculateAll();
+
+      // Guy has 10 wis and is wearing a +2 wis bracer
+      assert.equal(subject.getParameter('accumulation:ability_scores:wisdom'), 12);
+    });
+
+    it('should be able to de-dupe non-stacking bonuses', function() {
+      const subject = CharacterData.getTemplate();
+
+      const bunch_of_gear = [
+        {
+          slot: 'wrists',
+          item: {
+            key: 'asdfqwertyyuo',
+            slot: 'wrists',
+            name: 'Amulet of Wisdom (+2)',
+            type: 'equipment',
+            size: 'S',
+            adjustments: [
+              {attribute: 'wisdom', type: 'enhancement', value: 2}
+            ],
+            tags: [],
+          },
+        },
+        {
+          slot: 'face...',
+          item: {
+            key: 'aoweifjwaoeifjaoweifj',
+            slot: 'wrists',
+            name: 'Amulet of Wisdom (+3)',
+            type: 'equipment',
+            size: 'S',
+            adjustments: [
+              {attribute: 'wisdom', type: 'enhancement', value: 3}
+            ],
+            tags: [],
+          },
+        }
+      ];
+
+      subject.setParameter('equipment', bunch_of_gear);
+
+      // With 2 bracers, the one with the HIGHER wis score wins
+      assert.equal(subject.getParameter('accumulation:ability_scores:wisdom'), 13);
+    });
+  });
 });
