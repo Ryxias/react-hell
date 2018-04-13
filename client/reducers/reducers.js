598ff386e775c963dbe68e7f7183dbec74ee061f
Derek Wang <derek@thirdimpact.co>
2018-04-13 15:45:32 +0900

    Refactor Gossip to Ducks

--- client/reducers/reducers.js
+++ client/reducers/reducers.js
@@ -6,7 +6,7 @@ const rootReducer = combineReducers({
   gacha: require('../modules/gacha').default,
   user: require('./user_reducer'),
   alert: require('../modules/alert').default,
-  gossip: require('./gossip_reducer'),
+  gossip: require('../modules/gossip').default,
   clock: require('./clock_reducer').clockReducer,
   dice: require('../modules/dice').default,
 });
