213e05ecb69d3bf336658e6cef47d30f09844599
Derek Wang <derek@thirdimpact.co>
2018-04-13 11:46:58 +0900

    Operation manual for offline development

--- bin/sync_models.js
+++ bin/sync_models.js
@@ -9,7 +9,15 @@ const kernel = new AppKernel('production');
 
 kernel.boot();
 
-kernel.getContainer().get('ConnectionManager').sync()
+const ConnectionManager = kernel.getContainer().get('ConnectionManager');
+const SessionStore = kernel.getContainer().get('express.session_store');
+
+ConnectionManager.createDatabaseIfNotExists()
+  // create Sessions table
+  .then(() => SessionStore.sync())
+  // This step may fail because the models are sync'd in the wrong order (Foreign key problems)
+  // Simply re-run the whole operation and it should work
+  .then(() => ConnectionManager.sync())
   .then(() => console.log('Sync completed!'))
   .catch(err => {
     console.error(`Sync error:`);
