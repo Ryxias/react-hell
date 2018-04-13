0d9548f425fba9faf7744e00260a2b83f1e6ae6c
Derek Wang <derek@thirdimpact.co>
2018-04-14 00:38:55 +0900

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,10 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 const NODE_ENV = process.env.NODE_ENV;
 
+//
+// Webpack bundle analyzer
+//  lets us examine what's taking up so much space in the webpack bundle
+//
 const { analyzer_mode, analyzer_port } = (() => {
   const ENABLE = false; // set this to true and it will boot up the bundle analyzer
   if (!ENABLE || NODE_ENV === 'production') {
@@ -13,6 +17,54 @@ const { analyzer_mode, analyzer_port } = (() => {
   return { analyzer_mode: 'server', analyzer_port: 8001 };
 })();
 
+//
+// Webpack Development tools
+//
+// There's an excellent blog post that compares the various dev tools:
+//   http://cheng.logdown.com/posts/2016/03/25/679045
+//
+// In a nutshell:
+//   source-map:
+//     Generates a .map source map file.  Significant filesize.  For emitted errors, filename and
+//     line numbers displayed properly.  File source incorrect.
+//
+//   cheap-source-map:
+//     Map file.  Small filesize.  Bad for debugging, as only the filename displays properly.
+//
+//   cheap-module-source-map:
+//     Map file.  Very small filesize.  Both filename and line number displays properly.  Bad
+//     for debugging.
+//     Ideal for dev if you don't make mistakes that require super deep debugging!
+//
+//   eval:
+//     Instead of creating a separate .map file for the source map, it folds it into the bundle.
+//     Increase in filesize (moderate).  Filename is correct but line number is not; it displays
+//     transpiled line numbers.
+//
+//   eval-source-map:
+//     Significant increase in bundle filesize.  Filename, lines, and source all correct.
+//
+//   cheap-module-eval-source-map:
+//     Significant increase in bundle filesize.  Elements all correct, but slightly cheaper than eval-source-map.
+//     Ideal for HEAVY debugging on dev.
+//
+//   false:
+//     This option neither generates a .map file nor embeds the source map.  This results in no
+//     additional filesize.  When an error is encountered, it just says "error in line 2374823 in bundle.js"
+//     which is completely unhelpful.
+//     Ideal for production.
+
+const devtool = (() => {
+  // if (NODE_ENV === 'production') {
+  //   return false;
+  // }
+
+  return false;
+  //return 'eval-source-map';
+})();
+
+
+//
 module.exports = {
   entry: './client/index.jsx', // should be the main index.jsx of the client
   output: {
@@ -25,7 +77,7 @@ module.exports = {
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets:[ 'es2015', 'react', 'stage-2' ] } }
     ]
   },
-  devtool: 'source-map',  // easier debugging of de-minified code in browser
+  devtool: devtool,
   plugins: [
     new BundleAnalyzerPlugin({
       // Can be `server`, `static` or `disabled`.
