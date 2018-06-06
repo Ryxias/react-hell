f4ce2a6d087632eb1766f5dca05a7dfae1ccb0cd
Derek Wang <derek@thirdimpact.co>
2018-06-06 14:43:10 -0700

    Bring back sync models.

---
+++ bin/sync_models.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//
+// Synchronizes the database schema with the models that have been loaded
+//
+
+const AppKernel = require('../app/AppKernel');
+const kernel = new AppKernel(process.env.NODE_ENV);
+
+kernel.boot();
+
+kernel.getContainer().get('ConnectionManager').sync()
+  .then(() => console.log('Sync completed!'))
+  .then(() => process.exit(0))
+  .catch(err => console.error(`Sync error: ${err.message}.`));
